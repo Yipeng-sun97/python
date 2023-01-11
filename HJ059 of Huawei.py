20115c21369922ad427b9d38a2be2e59ce70c554
sampson <70687096+Yipeng-sun97@users.noreply.github.com>
2023-01-11 12:01:24 -0700

    Update HJ059 of Huawei.py

--- HJ059 of Huawei.py
+++ HJ059 of Huawei.py
@@ -28,3 +28,18 @@ for k,v in dic.items():
         break
 else:
     print(-1)
+    
+######solution#####
+while True:
+    try:
+        s = input()
+
+        for i in s:
+            if s.count(i) == 1:
+                print(i)
+                break
+        else:
+            print('-1')
+    except:
+        break
+
